067d50e58b5555b2c362978482defb11cb7b5ae0
JCEEE <0xjceee@proton.me>
2025-02-26 13:07:41 +0000

    add all for publish test

---
+++ index.js
@@ -0,0 +1,29 @@
+import { readFileSync, existsSync } from "fs";
+import PocketBase from "pocketbase";
+const pb = new PocketBase("https://pb.cycocyan.xyz");
+const METAFILE = "./meta.json";
+const { EMAIL, PASSWORD } = process.env;
+
+const authData = await pb
+  .collection("_superusers")
+  .authWithPassword(EMAIL, PASSWORD);
+
+if (existsSync(METAFILE)) {
+  const raw = readFileSync(METAFILE, "utf-8");
+  let data = JSON.parse(raw);
+  const request = `title="${data.title}"`;
+  try {
+    const record = await pb
+      .collection("Microsite_Meta")
+      .getFirstListItem(request);
+    await pb.collection("Microsite_Meta").update(record.id, { ...data });
+  } catch (error) {
+    if (error.status === 404)
+      await pb.collection("Microsite_Meta").create(data);
+  }
+
+  // logout
+  pb.authStore.clear();
+}
+
+// // "mrjcnicolas@gmail.com", "lamepass"
